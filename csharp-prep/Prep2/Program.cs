59dd5e72678a90737f3abe16444440222d3dbf32
Okothhillary <hobboltport@gmail.com>
2023-09-16 12:46:22 +0300

    grade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade:");
+        string response = Console.ReadLine();
+        int gradeResponse = int.Parse(response);
+
+        string letter = "A";
+
+        if (gradeResponse > 90 || gradeResponse == 90)
+            {
+                letter = "A";
+            }
+            else if (gradeResponse > 80 || gradeResponse == 80)
+            {
+                letter ="B";
+            }
+            else if (gradeResponse > 70 || gradeResponse == 70)
+            {
+                letter = "C";
+            }
+            else if (gradeResponse > 60 || gradeResponse == 60)
+            {
+                letter = "D";
+            }
+            else if (gradeResponse < 60 )
+            {
+                letter = "F";
+            }
+
+        
+        
+        Console.WriteLine($"Your grade is {letter}.");    
+
+        if (gradeResponse < 70)
+            {
+                Console.WriteLine("You failed.");
+            }
+        else
+        {
+            Console.WriteLine("You passed.");
+        }
     }
 }
