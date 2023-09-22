8199ab19ae1b009b81e6baa704506c324c6f0c1e
Okothhillary <hobboltport@gmail.com>
2023-09-22 13:21:43 +0300

    functions in C#

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -25,10 +25,27 @@ class Program
 
             {                
                 sum += num;
-
             }
 
-        Console.WriteLine($"Sum of numbers in that list: {sum}");       
-       
+        Console.WriteLine($"Sum of numbers in that list: {sum}"); 
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        // There are several ways to do this, such as sorting the list
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
-}
+}      
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squareNumber);
     }
+
+    static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Here we Go!");            
+        }
+
+    static string PromptUserName()
+    {
+        Console.Write("Type Username: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Give me a number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        
+        return userNumber;
+    }
+    static int SquareNumber(int userNumber)
+    {
+        
+        int squareNumber = userNumber * userNumber;
+
+        return squareNumber;
+    }
+
+    static void DisplayResult(string userName, int squareNumber)
+    {
+        Console.WriteLine($"Hi, {userName}. Your number squared is{squareNumber}.");
+    }
+
 }
