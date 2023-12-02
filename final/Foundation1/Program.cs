4cf3488acd9bfe8f6e50916dedf1016819a008df
Okothhillary <hobboltport@gmail.com>
2023-12-02 20:43:19 +0300

    Abstraction and encapsulation

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Console.WriteLine("Video and Comments tracker programme");
+
+        Video myVideo = new Video("Introduction to C#", "Brother Paul", 300);
+        Comment comment1 = new Comment("Alice", "Great video!");
+        Comment comment2 = new Comment("Cheshire Cat", "I learned a lot.");
+
+        myVideo.AddComment(comment1);
+        myVideo.AddComment(comment2);
+
+        myVideo.DisplayInfo();
+
+
+
+        
     }
 }
